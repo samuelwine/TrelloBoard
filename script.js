1a2347d2299c90cc8fc5a0672e5bed4332a62d60
Itzik Fekete <myfekete@gmail.com>
2018-03-06 15:19:50 +0000

    acceped Shmuel's changes

--- script.js
+++ script.js
@@ -4,17 +4,16 @@
 // the function is how we get the button to adda new line
 function getItem() {
     var nameField = document.getElementById('todow').value;
-    var result = document.getElementById('result');
-// I don't really need the f only the else but, as I can't think of a parameter to use, I used this but I am open for changes
+    var resultContainer = document.createElement('li');
+    var taskList = document.querySelector(".todolist");
+    taskList.appendChild(resultContainer);
 
-    if (nameField.length < 3) {
-        result.textContent = 'Username must contain at least 3 characters';
-        
-    } else {
-        // textContent is how the line is actually being added
-        result.textContent =  nameField;
-    }
+    // textContent is how the line is actually being added
+    resultContainer.textContent = nameField;
+
+    document.getElementById('todow').value = "";
 }
+
 // use an eventlistener for the event
 var subButton1 = document.getElementById('submit1');
 subButton1.addEventListener('click', getItem, false);
