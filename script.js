0067d96fbf57ac58462dd37ccee5ae3db858b0b2
Itzik Fekete <myfekete@gmail.com>
2018-03-07 09:39:34 +0000

    added button

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 // test 
 //shmuels change
 
-// the function is how we get the button to add a new line
+// the function is how we get the button to adda new line
 function getItem() {
     var nameField = document.getElementById('todow').value;
     var resultContainer = document.createElement('li');
     var taskList = document.querySelector(".todolist");
-
+    taskList.appendChild(resultContainer);
 
     // textContent is how the line is actually being added
     resultContainer.textContent = nameField;
 // the next 3 lines are for a button to appear 
     document.getElementById('todow').value = "";
-    var doing1 = document.createElement('input');
-    doing1.setAttribute('type', 'button');
+    var doing1 = document.createElement('input')
+var doingText = doing1.setAttribute('type', 'button');
+
     // the line below could have been done without a var but created a var to be able to be used at the next stage
     var submited1 = resultContainer.appendChild(doing1); 
 }
-
- 
-        // textContent is how the line is actually being added
-        resultContainer.textContent =  nameField;
-    }
 
 // use an eventlistener for the event
 var subButton1 = document.getElementById('submit1');
